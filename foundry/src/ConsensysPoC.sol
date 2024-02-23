9bdfcd2b669cea3db3fee57a671c0687aeb4084f
Pedrojok01 <pedrojok@pm.me>
2024-02-23 12:38:15 +0700

    🔄 refactor(.env.example): replace ETHERSCAN_API_KEY with LINEASCAN_API_KEY to switch explorer service ✨ feat(ConsensysPoC.sol): add ERC721Enumerable extension to the contract for token enumeration 🔧 fix(ConsensysPoC.sol): move tokenURI function to public scope for external access 🔧 fix(ConsensysPoC.sol): add _update, _increaseBalance, supportsInterface overrides for compatibility with ERC721Enumerable

--- foundry/src/ConsensysPoC.sol
+++ foundry/src/ConsensysPoC.sol
@@ -2,20 +2,46 @@
 pragma solidity 0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract ConsensysPoC is ERC721 {
+contract ConsensysPoC is ERC721, ERC721Enumerable {
     string private constant TOKEN_URI =
         "ipfs://QmbdHSfasCqAmwcGGKDqp23aMSY5DvGiRZy4cq1tbGEm5t/metadata.json";
     uint256 private _nextTokenId;
 
     constructor() ERC721("ConsensysPoC", "CPC") {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return TOKEN_URI;
-    }
-
     function safeMint(address to) public {
         uint256 tokenId = ++_nextTokenId;
         _safeMint(to, tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        _requireOwned(tokenId);
+
+        return TOKEN_URI;
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
